e4583b2b12c653a896fc945571ee888d7209c95b
xtrphy <perepeckinvlad@gmail.com>
2025-05-11 21:24:08 +0500

    cors updates

--- api/app.js
+++ api/app.js
@@ -2,6 +2,14 @@ const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 
+const authenticateToken = require('./middlewares/authMiddleware');
+const registerRouter = require('./routes/register');
+const loginRouter = require('./routes/login');
+const postsRouter = require('./routes/posts');
+const postRouter = require('./routes/post');
+
+const app = express();
+
 const allowedOrigins = [
     'http://localhost:5173',
     'https://resonant-starship-05112e.netlify.app'
@@ -18,17 +26,6 @@ app.use(cors({
     credentials: true
 }));
 
-const authenticateToken = require('./middlewares/authMiddleware');
-const registerRouter = require('./routes/register');
-const loginRouter = require('./routes/login');
-const postsRouter = require('./routes/posts');
-const postRouter = require('./routes/post');
-
-const app = express();
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}));
 app.use(express.json());
 
 
